e1006b7ca4c0cfb88d4dfb30a43bb85ba400240b
zhaochang_jiang <zhaochang_jiang_ext@aisino-wincor.com>
2020-01-10 13:46:41 +0800

    Initial commit

---
+++ src/main/java/com/jzc/entity/TestData.java
@@ -0,0 +1,4 @@
+package com.jzc.entity;
+
+public class TestData {
+}
---
+++ src/test/java/com/jzc/algorithm_visualization/AlgorithmVisualizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzc.algorithm_visualization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmVisualizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
